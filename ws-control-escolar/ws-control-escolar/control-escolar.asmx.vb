21d6ed93d82fa3dab613b345854c2434c414da20
ezarria <ezarria@yahoo.com>
2021-01-29 19:01:28 -0500

    Segunda Version

---
+++ ws-control-escolar/ws-control-escolar/control-escolar.asmx.vb
@@ -0,0 +1,26 @@
+﻿Imports System.ComponentModel
+Imports System.Web.Services
+Imports System.Web.Services.Protocols
+
+' Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente.
+' <System.Web.Script.Services.ScriptService()> _
+<System.Web.Services.WebService(Namespace:="http://tempuri.org/")> _
+<System.Web.Services.WebServiceBinding(ConformsTo:=WsiProfiles.BasicProfile1_1)> _
+<ToolboxItem(False)> _
+Public Class control_escolar
+    Inherits System.Web.Services.WebService
+
+    <WebMethod()>
+    Public Function HelloWorld() As String
+        Return "Hola a todos"
+    End Function
+    <WebMethod()>
+    Public Function AlumnoGuardar() As Boolean
+        Try
+            Return True
+        Catch ex As Exception
+            Return False
+        End Try
+    End Function
+
+End Class
